1f7c7b636195c21ec0206d979bfdad2d3eaf8cb2
Роман Бухмистов <rrb13@mail.ru>
2022-10-13 18:08:08 +0300

    задания 1-3

--- dz1.py
+++ dz1.py
@@ -10,3 +10,95 @@
 a) Добавьте игру против бота
 b) Подумайте как наделить бота "интеллектом"'''
 
+
+print('Программа для игры с конфетами человек против человека.')
+
+from random import randint
+
+def makeMove(name):
+    x = int(input(f"{name}, введите количество конфет, которое возьмете от 1 до 28: "))
+    while x < 1 or x > 28:
+        x = int(input(f"{name}, введите корректное количество конфет: "))
+    return x
+
+
+def p_print(name, k, counter, quantity):
+    print(f"{name} взял {k}, теперь их у вас {counter}. Осталось на столе {quantity} конфет.")
+
+player1 = input("Введите имя первого игрока: ")
+player2 = input("Введите имя второго игрока: ")
+quantity = 2021
+flag = randint(0,1)
+print('Жеребьевка:')
+if flag:
+    print(f"Первым ходит игрок {player1}")
+else:
+    print(f"Первым ходит игрок {player2}")
+
+counter1 = 0 
+counter2 = 0
+
+while quantity > 28:
+    if flag:
+        k = makeMove(player1)
+        counter1 += k
+        quantity -= k
+        flag = False
+        p_print(player1, k, counter1, quantity)
+    else:
+        k = makeMove(player2)
+        counter2 += k
+        quantity -= k
+        flag = True
+        p_print(player2, k, counter2, quantity)
+
+if flag:
+    print(f"Поздравляю {player1}, вы выиграли!")
+else:
+    print(f"Поздравляю {player2}, вы выиграли!")
+
+print('Программа для игры с конфетами человек против бота.')
+
+from random import randint
+
+def makeMove(name):
+    x = int(input(f"{name}, введите количество конфет, которое возьмете от 1 до 28: "))
+    while x < 1 or x > 28:
+        x = int(input(f"{name}, введите корректное количество конфет: "))
+    return x
+
+
+def p_print(name, k, counter, quantity):
+    print(f"{name} взял {k}, теперь их у него {counter}. Осталось на столе {quantity} конфет.")
+
+player1 = input("Введите ваше имя: ")
+player2 = "Bot"
+quantity = 2021
+flag = randint(0,1) 
+print('Жеребьевка:')
+if flag:
+    print(f"Первым ходит игрок {player1}")
+else:
+    print(f"Первым ходит игрок {player2}")
+
+counter1 = 0 
+counter2 = 0
+
+while quantity > 28:
+    if flag:
+        k = makeMove(player1)
+        counter1 += k
+        quantity -= k
+        flag = False
+        p_print(player1, k, counter1, quantity)
+    else:
+        k = randint(1,29)
+        counter2 += k
+        quantity -= k
+        flag = True
+        p_print(player2, k, counter2, quantity)
+
+if flag:
+    print(f"Поздравляю {player1}, вы победили бота!")
+else:
+    print(f"Выиграл {player2}")
--- dz2.py
+++ dz2.py
@@ -1,2 +1,34 @@
 '''задача 2. Реализуйте RLE алгоритм: реализуйте модуль сжатия и восстановления данных.
 Входные и выходные данные хранятся в отдельных текстовых файлах.'''
+
+def rle_code(data):
+    encoding = ""
+    prev_char = ""
+    count = 0
+    if not data:
+        return ""
+    for char in data:
+        if char != prev_char:
+            encoding += prev_char + str(count)
+            count = 1
+            prev_char = char
+        else:
+            count += 1
+    else:
+        encoding += prev_char + str(count)
+    pos = 0
+    encoding = encoding[0:pos] + encoding[pos + 1 :]
+    return encoding
+    print()
+
+var_1 = input("Введите текст (пример aaaaaabbbbbcccccc):\n")
+with open("Rle01.txt", "w") as data:
+    data.write(var_1)
+with open("Rle01.txt", "r") as f:
+    enter = f.read()
+
+new_val_rle = rle_code(enter)
+print(new_val_rle)
+
+with open("Rle02.txt", "w") as data:
+    data.write(new_val_rle)
